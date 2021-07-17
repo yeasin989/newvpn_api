4a8e7e46c252f487aa36d102703c835537ae80c9
yeasin989 <miahmohammedyeasin@gmail.com>
2021-07-17 21:43:23 +0300

    my frist

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb://newvpn:dc2u2HJi622tEp7J@cluster0-shard-00-00.ppkvo.mongodb.net:27017,cluster0-shard-00-01.ppkvo.mongodb.net:27017,cluster0-shard-00-02.ppkvo.mongodb.net:27017/newvpn?ssl=true&replicaSet=atlas-q6ychh-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
